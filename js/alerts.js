a7a5eeec61cb2384a7e8fe07635c09862797471e
Gsantiago31 <84323828+Gsantiago31@users.noreply.github.com>
2022-07-07 23:36:49 -0600

    first commit

---
+++ js/alerts.js
@@ -0,0 +1,23 @@
+let alertnofunction = document.querySelector('.nofunction');
+
+alertnofunction.addEventListener('click', () => {
+    alert ("Lo lamentamos, pronto tendremos esta  opción.");
+});
+
+let alertnofunction1 = document.querySelector('.nofunction1');
+
+alertnofunction1.addEventListener('click', () => {
+    alert ("Lo lamentamos, pronto tendremos esta  opción.");
+});
+
+let alertnofunction2 = document.querySelector('.nofunction2');
+
+alertnofunction2.addEventListener('click', () => {
+    alert ("Lo lamentamos, pronto tendremos esta  opción.");
+});
+
+let alertnofunction3 = document.querySelector('.nofunction3');
+
+alertnofunction3.addEventListener('click', () => {
+    alert ("Lo lamentamos, pronto tendremos esta  opción.");
+});
